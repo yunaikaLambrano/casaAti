e4cbada78c9123d9652a7e892efcd0740d922f09
yunaikaLambrano <yunaikadealo25@gmail.com>
2026-06-04 12:04:42 -0500

    Recursos

---
+++ Assets/CASAATI_CODIGOS/Navegacion.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfazReal : MonoBehaviour
+{
+    public void IrInicio()
+    {
+        print("inicio");
+        SceneManager.LoadScene("inicio");
+    }
+
+    public void IrEscena1()
+    {
+        print("escaner");
+        SceneManager.LoadScene("escaner");
+    }
+
+    public void IrEscena2()
+    {
+        print("info_pilon");
+        SceneManager.LoadScene("info_pilon");
+    }
+    public void IrEscena3()
+    {
+        print("grupo_laborArtesanal");
+        SceneManager.LoadScene("grupo_laborArtesanal");
+    }
+    public void IrEscena4()
+    {
+        print("gruposTodos ");
+        SceneManager.LoadScene("gruposTodos");
+    }
+
+
+}
